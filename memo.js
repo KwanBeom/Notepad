5e7c262a553468611421751dece41b2239f64ec4
BK <3031011@naver.com>
2023-04-24 03:53:52 +0900

    20230424 수정

--- memo.js
+++ memo.js
@@ -17,28 +17,14 @@ const memoBtn = document.querySelector(".memo-btn"),
     memoSection = document.querySelector(".savememo-section"),
     postText = document.querySelector(".toastui-editor-contents"),
     titleText = document.querySelector(".title"),
-    d = new Date(),
-    onfSwitch = document.querySelector(".onf-switch")
+    onfSwitch = document.querySelector(".onf-switch"),
+    resetBtn = document.querySelector("[type='reset']")
     
 
-let allMemo = JSON.parse(localStorage.getItem("allMemo")),
-    delBtns,
-    oldFirst = false
+let allMemo = JSON.parse(localStorage.getItem("allMemo"))
 allMemo = allMemo ?? []
 
 paintMemo()
-
-// onoff 스위치로 클릭시 최신, 오래된 순 정렬하는 이벤트
-onfSwitch.onclick = function(){
-    onfSwitch.classList.toggle("on")
-    if(onfSwitch.classList.contains("on")){
-        allMemo.reverse()
-    }else{
-        allMemo.reverse()
-    }
-    memoSection.innerHTML = ''
-    paintMemo()
-}
 
 // 작성하기 버튼 눌렀을시에 로컬 스토리지 내용을 HTML에 뿌려주는 함수
 function post() {
@@ -56,7 +42,8 @@ function post() {
 
 // 로컬 스토리지에 제목/내용 저장하는 함수
 function inputMemo(){
-    const {...memo} = {title: titleText.value, posttext: postText.innerHTML, time: `${d.getFullYear()}년 ${d.getMonth()+1}월 ${d.getDate()}일 ${d.getHours()}시${d.getMinutes()}분`, val: 0}
+    const d = new Date(),
+        {...memo} = {title: titleText.value, posttext: postText.innerHTML, time: `${d.getFullYear()}년 ${d.getMonth()+1}월 ${d.getDate()}일 ${d.getHours()}시${d.getMinutes()}분`, val: 0}
     if(onfSwitch.classList.contains("on")){
         allMemo.push({...memo})
     } else {
@@ -66,9 +53,9 @@ function inputMemo(){
     localStorage.setItem('allMemo', JSON.stringify(allMemo))
 }
 
-// 로컬 스토리지 내용 HTML에 넣어주는 함수
+// 로컬 스토리지 프로퍼티를 HTML에 넣어주는 함수
 function paintMemo(){
-    allMemo.forEach(item => {
+    allMemo.forEach((item, idx) => {
         memoSection.insertAdjacentHTML("beforeend", 
         `
         <article class="memo-wrap">
@@ -77,7 +64,7 @@ function paintMemo(){
                 <time>${item['time']}</time>
             </header>
             <p>${item['posttext']}</p>
-            <button class="del-btn" data-val="${item.val}">X</button>
+            <button class="del-btn" data-val="${idx}">X</button>
         </article>
         `
         )
@@ -86,7 +73,14 @@ function paintMemo(){
 
 }
 
-// 
+// 순서대로 allMemo 배열 val키에 index 값 넣어주는 함수
+function idxSet(){
+    allMemo.forEach((item, idx) => {
+        item.val = idx
+    })
+}
+
+// 배열 삭제해서 리턴해주는 함수
 memoSection.addEventListener("click", function(e){
     if(e.target.dataset.val !== undefined){
         dele(e.target.dataset.val)
@@ -96,7 +90,7 @@ memoSection.addEventListener("click", function(e){
     idxSet()
 })
 
-// 배열 삭제해서 리턴해주는 함수
+
 function dele(val) {
     allMemo = allMemo.filter(item => {
         return item.val != val
@@ -104,11 +98,23 @@ function dele(val) {
     localStorage.setItem('allMemo', JSON.stringify(allMemo))
 }
 
-// 순서대로 val 키에 index 값 넣어주는 함수
-function idxSet(){
-    allMemo.forEach((item, idx) => {
-        item.val = idx
-    })
+// onoff 스위치로 클릭시 최신, 오래된 순 정렬하는 이벤트
+onfSwitch.onclick = () => {
+    onfSwitch.classList.toggle("on")
+    if(onfSwitch.classList.contains("on")){
+        allMemo.reverse()
+    }else{
+        allMemo.reverse()
+    }
+    memoSection.innerHTML = ''
+    paintMemo()
+}
+
+// 모든 메모 삭제 이벤트
+resetBtn.onclick = () => {
+    allMemo = [];
+    localStorage.setItem('allMemo', JSON.stringify(allMemo))
+    memoSection.innerHTML = ''
 }
 
 
